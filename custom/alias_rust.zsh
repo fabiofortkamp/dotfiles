5f242b81c4b02c1815e8b8e5078cd8e5ff367709
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2023-05-17 10:44:07 -0300

    Fix bug in setting aliases.

--- custom/alias_rust.zsh
+++ custom/alias_rust.zsh
@@ -5,10 +5,10 @@
 
 # https://stackoverflow.com/questions/7522712/how-can-i-check-if-a-command-exists-in-a-shell-script
 
-if ! type "$exa" > /dev/null; then
+if type "$exa" > /dev/null; then
   alias ls="exa --icons"
 fi
 
-if ! type "$bat" > /dev/null; then
+if type "$bat" > /dev/null; then
   alias cat="bat --style=auto"
 fi
