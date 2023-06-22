69b8b48446c5c1ca65b7032920daf53edb5250ad
Fábio Fortkamp <fabio@fabiofortkamp.com>
2023-06-22 11:28:05 -0300

    Add more configuration for Flake8 in LunarVim

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -6,7 +6,10 @@ local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup{{name = "black"},}
 
 local linters = require "lvim.lsp.null-ls.linters"
-linters.setup { { command = "flake8", args = {"--ignore=E230", filetypes = {"python"}}}}
+linters.setup { { command = "flake8", args = {
+  "--ignore=E203", -- whitespace before ‘,’, ‘;’, or ‘:’
+  "--max-line-length=88", -- for black compatibility
+  filetypes = {"python"}}}}
 
 lvim.plugins = {
   "AckslD/swenv.nvim",
