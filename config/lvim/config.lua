45ead6d7914557c92be7110d5253b33bbf2ae733
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2023-06-23 14:47:52 -0300

    Add pyright to lvim config

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -27,3 +27,24 @@ lvim.builtin.which_key.mappings["C"] = {
   c = {"<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env"}
 }
 
+local pyright_opts = {
+  single_file_support = true,
+  settings = {
+    pyright = {
+      disableLanguageServices = false,
+      disableOrganizeImports = false
+    },
+    python = {
+      analysis = {
+        autoImportCompletions = true,
+        autoSearchPaths = true,
+        diagnosticMode = "workspace", -- openFilesOnly, workspace
+        typeCheckingMode = "basic", -- off, basic, strict
+        useLibraryCodeForTypes = true
+      }
+    }
+  },
+}
+
+require("lvim.lsp.manager").setup("pyright", pyright_opts)
+
