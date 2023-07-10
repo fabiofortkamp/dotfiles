adc344333ac8ce0b811b8d458689c34d26e66194
Fábio Fortkamp <fabio@fabiofortkamp.com>
2023-07-09 21:25:36 -0300

    Add python testing lvim config.

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -15,7 +15,11 @@ linters.setup { { command = "flake8", args = {
 lvim.plugins = {
   "AckslD/swenv.nvim",
   "stevearc/dressing.nvim",
-"JuliaEditorSupport/julia-vim"}
+  "mfusseneger/nvim-dap-python",
+  "nvim-neotest/neotest",
+  "nvim-neotest/neotest-python",
+  "JuliaEditorSupport/julia-vim"}
+
 
 require("swenv").setup({
   post_set_venv = function()
@@ -23,9 +27,43 @@ require("swenv").setup({
   end,
 })
 
+- setup debug adapter
+lvim.builtin.dap.active = true
+local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+pcall(function()
+  require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
+end)
+
+-- setup testing
+require("neotest").setup({
+  adapters = {
+    require("neotest-python")({
+      -- Extra arguments for nvim-dap configuration
+      -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+      dap = {
+        justMyCode = false,
+        console = "integratedTerminal",
+      },
+      args = { "--log-level", "DEBUG", "--quiet" },
+      runner = "pytest",
+    })
+  }
+})
+
+lvim.builtin.which_key.mappings["dm"] = { "<cmd>lua require('neotest').run.run()<cr>",
+  "Test Method" }
+lvim.builtin.which_key.mappings["dM"] = { "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>",
+  "Test Method DAP" }
+lvim.builtin.which_key.mappings["df"] = {
+  "<cmd>lua require('neotest').run.run({vim.fn.expand('%')})<cr>", "Test Class" }
+lvim.builtin.which_key.mappings["dF"] = {
+  "<cmd>lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<cr>", "Test Class DAP" }
+lvim.builtin.which_key.mappings["dS"] = { "<cmd>lua require('neotest').summary.toggle()<cr>", "Test Summary" }
+
+
 lvim.builtin.which_key.mappings["C"] = {
   name = "Python",
-  c = {"<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env"}
+   c = {"<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env"}
 }
 
 local pyright_opts = {
