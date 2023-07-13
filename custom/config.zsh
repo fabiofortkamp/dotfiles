acbd2daffb39151e06af352bc5b9ad2cd44b970c
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2023-07-13 11:56:39 -0300

    Add editing aliases.

--- custom/config.zsh
+++ custom/config.zsh
@@ -1,1 +1,5 @@
-export XDG_CONFIG_HOME=$HOME/.config/
+export XDG_CONFIG_HOME="$HOME/.config"
+
+alias elvim='$EDITOR $XDG_CONFIG_HOME/lvim'
+alias envim='$EDITOR $XDG_CONFIG_HOME/nvim'
+alias elocal='$EDITOR $HOME/dotfiles/custom/local.zsh'
