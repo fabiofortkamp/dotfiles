244400cf985c4cb2258fc5d11f454943b84b41dc
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2023-07-18 16:32:34 -0300

    Replace lazygit alias with function
    This function was recommended in lazygit official repo.
    If we switch repos inside lazygit, when aborting the directory is retained
    in the shell.

--- custom/plugins/lazygit/lazygit.plugin.zsh
+++ custom/plugins/lazygit/lazygit.plugin.zsh
@@ -1,1 +1,11 @@
-alias lg='lazygit'
+lg()
+{
+    export LAZYGIT_NEW_DIR_FILE=~/.lazygit/newdir
+
+    lazygit "$@"
+
+    if [ -f $LAZYGIT_NEW_DIR_FILE ]; then
+            cd "$(cat $LAZYGIT_NEW_DIR_FILE)"
+            rm -f $LAZYGIT_NEW_DIR_FILE > /dev/null
+    fi
+}
