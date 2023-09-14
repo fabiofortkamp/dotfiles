18e9d90ce02111da17722206d812cb9d65d2d36e
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2023-09-14 08:44:51 -0300

    Add stack trace package to julia.

--- julia/config/startup.jl
+++ julia/config/startup.jl
@@ -1,3 +1,15 @@
+
+# ensuring packages loaded in startup are installed
+import Pkg
+let
+    pkgs = ["Revise", "OhMyREPL", "DrWatson", "AbbreviatedStackTraces"]
+    for pkg in pkgs
+        if Base.find_package(pkg) === nothing
+            Pkg.add(pkg)
+        end
+    end
+end
+
 try
     @eval using Revise
 catch e
@@ -13,3 +25,5 @@ atreplinit() do repl
 end
 
 ENV["JULIA_EDITOR"] = "code-insiders"
+using AbbreviatedStackTraces
+ENV["JULIA_STACKTRACE_MINIMAL"] = true
