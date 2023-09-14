c06e7f69df3664b24b679c2334913a23c9932eb5
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2023-09-14 08:50:25 -0300

    Modify Julia REPL

--- julia/config/startup.jl
+++ julia/config/startup.jl
@@ -24,6 +24,43 @@ atreplinit() do repl
     end
 end
 
+# some contributions from Eduardo Alves below:
+
+import REPL
+
+# Exit with :q and restart with :r
+pushfirst!(REPL.repl_ast_transforms, function (ast::Union{Expr,Nothing})
+    function toplevel_quotenode(ast, s)
+        return (Meta.isexpr(ast, :toplevel, 2) && ast.args[2] === QuoteNode(s)) ||
+               (Meta.isexpr(ast, :toplevel) && any(x -> toplevel_quotenode(x, s), ast.args))
+    end
+    if toplevel_quotenode(ast, :q)
+        exit()
+    elseif toplevel_quotenode(ast, :r)
+        argv = Base.julia_cmd().exec
+        opts = Base.JLOptions()
+        if opts.project != C_NULL
+            push!(argv, "--project=$(unsafe_string(opts.project))")
+        end
+        if opts.nthreads != 0
+            push!(argv, "--threads=$(opts.nthreads)")
+        end
+        # @ccall execv(argv[1]::Cstring, argv::Ref{Cstring})::Cint
+        ccall(:execv, Cint, (Cstring, Ref{Cstring}), argv[1], argv)
+    end
+    return ast
+end)
+
+
+
 ENV["JULIA_EDITOR"] = "code-insiders"
+
+# make error messages shorter
 using AbbreviatedStackTraces
 ENV["JULIA_STACKTRACE_MINIMAL"] = true
+
+# make REPL colorful
+using OhMyREPL
+colorscheme!("BoxyMonokai256")
+enable_autocomplete_brackets(true)
+OhMyREPL.Passes.RainbowBrackets.activate_256colors()
