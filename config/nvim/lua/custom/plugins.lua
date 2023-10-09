a3ad43fb419cae29a467443988ea53f1550c723c
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2023-10-09 14:15:54 -0300

    Fix typo and document nvim configuration

--- config/nvim/lua/custom/plugins.lua
+++ config/nvim/lua/custom/plugins.lua
@@ -1,11 +1,15 @@
 -- table of plugins that are returned and loaded by chadrc.lua
 -- keep in mind that, in Lua, 'tables' can be 1D arrays or dicts;
--- in the case below, we have a list of dicts
+-- in the case below, we have a list of dicts, with the GitHub repo
+-- as the 'head' of each dict; I suppose the keys are required by each plugin
 local plugins = {
   {
     "neovim/nvim-lspconfig",
     config = function ()
+      -- these are paths relative to the neovim init lua folder
+      -- this is the default one installed by nvChad
       require "plugins.configs.lspconfig"
+      -- and this is the one in the current repo
       require "custom.configs.lspconfig"
     end,
   },
