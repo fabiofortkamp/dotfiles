b26585b90daa8af32d1ef6cf82a9e4398e7cae54
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2023-10-09 13:06:13 -0300

    Remove nvim configuration

--- README.md
+++ README.md
@@ -41,11 +41,6 @@ Restart the shell and then run `./install-asdf.zsh && chmod +x install-lvim.zsh 
 
 VS Code Insiders is set as the default editor - unless in a SSH session, which uses regular `vim`.
 
-
-### Neovim config
-
-The neovim config is largely taken from [this repo](https://github.com/LunarVim/Neovim-from-scratch).
-
 ### prm
 
 This repo includes 
--- install-lvim.zsh
+++ install-lvim.zsh
@@ -3,5 +3,4 @@
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
 
 mkdir -p "$HOME/.config" && ln -s "$HOME/dotfiles/config/lvim" "$HOME/.config/lvim"
-ln -s "$HOME/dotfiles/config/nvim"  $HOME/.config/nvim
 mkdir -p "$HOME/.julia" && ln -s "$HOME/dotfiles/julia/config" "$HOME/.julia/config"
