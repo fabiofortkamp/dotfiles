3d38a99f692976d86d1b7445e7ecc94b6eced7d8
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2024-01-29 14:27:13 -0300

    Add quarto user installation to path

--- custom/macos.zsh
+++ custom/macos.zsh
@@ -1,6 +1,6 @@
 if [[ $(uname) == "Darwin" ]]; then
 	fpath+=/opt/homebrew/share/zsh/site-functions
-	PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
+	PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$HOME/Applications/quarto/bin/$PATH"
 
 	source ~/.iterm2_shell_integration.zsh
 fi
