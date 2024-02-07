1571e85c9bd17e672bd117fad7ace394a4deb447
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2024-02-07 15:59:19 -0300

    Include Hugo Path in macOS

--- custom/macos.zsh
+++ custom/macos.zsh
@@ -1,6 +1,6 @@
 if [[ $(uname) == "Darwin" ]]; then
 	fpath+=/opt/homebrew/share/zsh/site-functions
 	PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$HOME/Applications/quarto/bin:$PATH"
-
+path+="$HOME/Library/Application Support/Hugo/0.92.2"
 	source ~/.iterm2_shell_integration.zsh
 fi
