cfb3c3a450c944d34e9de34159fcd4f1140ada08
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2024-02-21 08:53:03 -0300

    Change zsh suggestion strategy to prioritize completions
    If history is prioritized, then I might get suggestions that are
    not applicable to the current directory.
    
    I need to check if this is more useful

--- custom/config.zsh
+++ custom/config.zsh
@@ -3,3 +3,4 @@ export XDG_CONFIG_HOME="$HOME/.config"
 alias elvim='$EDITOR $XDG_CONFIG_HOME/lvim'
 alias envim='$EDITOR $XDG_CONFIG_HOME/nvim'
 alias elocal='$EDITOR $HOME/dotfiles/custom/local.zsh'
+ZSH_AUTOSUGGEST_STRATEGY=(completion history)
