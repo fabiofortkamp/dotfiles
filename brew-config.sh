2a702d77b4c1b798e169c96c6c47cf6d4238f804
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2024-05-02 09:05:33 -0300

    Include git flow configuration

--- brew-config.sh
+++ brew-config.sh
@@ -16,3 +16,13 @@ read git_user_email
 $(brew --prefix)/bin/git config --global user.name "$git_user_name"
 $(brew --prefix)/bin/git config --global user.email "$git_user_email"
 
+# Set git flow configuration
+$(brew --prefix)/bin/git flow config set --global master "main"
+$(brew --prefix)/bin/git flow config set --global develop "develop"
+$(brew --prefix)/bin/git flow config set --global feature "feature/"
+$(brew --prefix)/bin/git flow config set --global hotfix "hotfix/"
+$(brew --prefix)/bin/git flow config set --global bugfix "bugfix/"
+$(brew --prefix)/bin/git flow config set --global release "release/"
+$(brew --prefix)/bin/git flow config set --global support "support/"
+$(brew --prefix)/bin/git flow config set --global versiontagprefix "v"
+
