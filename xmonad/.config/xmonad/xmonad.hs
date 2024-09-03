3e6f91893ab2f53d7946a20ff616a7e80ad9fec2
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2024-09-03 13:35:49 -0300

    Simplify XMonad configuration

--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -5,9 +5,5 @@ main :: IO ()
 main = xmonad $ ewmhFullscreen $ ewmh $ myConfig
 
 myConfig = def
-    { terminal    = "wezterm"
-    , modMask     = mod4Mask -- Rebind Mod to the Windows (Super) key
-    , borderWidth = 2
-    , normalBorderColor = "#000000"
-    , focusedBorderColor = "#ff0000"
+    { modMask     = mod4Mask -- Rebind Mod to the Windows (Super) key
     }
