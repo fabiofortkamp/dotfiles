acdff4122077c8f980d6205b996759904abd16cb
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2024-10-07 15:50:23 -0300

    Add Fortran tools

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -609,6 +609,7 @@ require('lazy').setup({
         basedpyright = {},
         rust_analyzer = {},
         julials = {},
+        fortls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -647,6 +648,8 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'fprettify',
+        'findent',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -699,6 +702,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        fortran = { 'fprettify' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
@@ -917,7 +921,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'fortran' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
