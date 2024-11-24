7d83647892d644c72b4950d9959841435470ead1
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2024-11-24 23:03:07 +0100

    Add vimtex

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -318,7 +318,15 @@ require('lazy').setup({
       },
     },
   },
-
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      -- vim.g.vimtex_view_method = 'zathura'
+    end,
+  },
   -- NOTE: Plugins can specify dependencies.
   --
   -- The dependencies are proper plugin specifications as well - anything
