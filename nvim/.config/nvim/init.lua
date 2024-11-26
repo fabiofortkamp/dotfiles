c48d4bef75c6f10dfce4163e1775b486e74edbe3
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2024-11-26 21:04:32 +0100

    Add MATLAB language server

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -610,6 +610,7 @@ require('lazy').setup({
         rust_analyzer = {},
         julials = {},
         fortls = {},
+        matlab_ls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
