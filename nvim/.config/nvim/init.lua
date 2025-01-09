4ffba589ffd8b395692d6941c1e24ac6fb66e980
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-01-09 15:57:28 +0100

    Extract colorscheme set to main file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -111,5 +111,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 require 'config.lazy'
 
+vim.cmd.colorscheme 'tokyonight-night'
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
