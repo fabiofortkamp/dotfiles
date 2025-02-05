76fffd1e97b99116b87f76e17c3e94e3b07dc2fd
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-02-05 10:58:31 +0100

    Configure Python LSP in lazyvim

---
+++ lazyvim/.config/lazyvim/lua/plugins/lsp.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {},
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "flake8",
+        "mypy",
+        "basedpyright",
+        "isort",
+        "black",
+      },
+    },
+  },
+  { "williamboman/mason-lspconfig.nvim", config = function() end },
+}
