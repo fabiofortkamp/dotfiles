858731b01f45ddd7d914cbb772b6dc0a8680d190
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-02-05 10:58:16 +0100

    Set lazyvim to use telescope and nvim-cmp

--- lazyvim/.config/lazyvim/lua/config/options.lua
+++ lazyvim/.config/lazyvim/lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.maplocalleader = ","
+-- LazyVim picker to use.
+-- Can be one of: telescope, fzf
+-- Leave it to "auto" to automatically use the picker
+-- enabled with `:LazyExtras`
+vim.g.lazyvim_picker = "telescope"
+
+-- LazyVim completion engine to use.
+-- Can be one of: nvim-cmp, blink.cmp
+-- Leave it to "auto" to automatically use the completion engine
+-- enabled with `:LazyExtras`
+vim.g.lazyvim_cmp = "nvim-cmp"
