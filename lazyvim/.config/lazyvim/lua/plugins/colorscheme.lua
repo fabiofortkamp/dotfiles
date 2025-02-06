5bf947b98d27a8a23bbe1c33f22d6486434979a9
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-02-06 07:57:07 +0100

    Add other themes to lazyvim

--- lazyvim/.config/lazyvim/lua/plugins/colorscheme.lua
+++ lazyvim/.config/lazyvim/lua/plugins/colorscheme.lua
@@ -14,6 +14,20 @@ return {
     end,
   },
   {
+    "comfysage/evergarden",
+    priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
+    opts = {
+      transparent_background = true,
+      variant = "medium", -- 'hard'|'medium'|'soft'
+      overrides = {}, -- add custom overrides
+    },
+  },
+  { "shaunsingh/nord.nvim" },
+  { "Tsuzat/NeoSolarized.nvim" },
+  { "sainnhe/everforest" },
+  { "phha/zenburn.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "solarized",
