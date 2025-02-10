a4db0135dd713af27ad2328ab280554e4954aa27
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-02-10 11:33:35 +0100

    Define keymaps to load files

--- lazyvim/.config/lazyvim/lua/config/keymaps.lua
+++ lazyvim/.config/lazyvim/lua/config/keymaps.lua
@@ -1,9 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set('n', '<space><space>x', '<cmd>source %<CR>')
-vim.keymap.set('n', '<space>x', ':.lua<CR>')
-vim.keymap.set('v', '<space>x', ':lua<CR>')
 
-vim.keymap.set("n","<M-j>", "<cmd>cnext<CR>")
-vim.keymap.set("n","<M-k>", "<cmd>cprev<CR>")
+vim.keymap.set("n", "<leader>cx", "<cmd>source %<CR>", { desc = "Source file" })
+vim.keymap.set("n", "<leader>cy", ":.lua<CR>", { desc = "Source line/region" })
+vim.keymap.set("v", "<leader>cy", ":lua<CR>", { desc = "Source line/region" })
+
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
