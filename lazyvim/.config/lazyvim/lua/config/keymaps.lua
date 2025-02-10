db3df2c91714cb2d941086dd6e045d38d35e9518
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-02-10 11:38:46 +0100

    Create terminal keybindings

--- lazyvim/.config/lazyvim/lua/config/keymaps.lua
+++ lazyvim/.config/lazyvim/lua/config/keymaps.lua
@@ -8,3 +8,25 @@ vim.keymap.set("v", "<leader>cy", ":lua<CR>", { desc = "Source line/region" })
 
 vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
 vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
+
+-- Terminal customizations
+-- floating terminal
+local map = vim.keymap.set
+local wk = require('which-key')
+wk.add({{ "<leader>t",  group = "Terminal" }})
+map("n", "<leader>tT", function()
+  Snacks.terminal()
+end, { desc = "Terminal (cwd)" })
+map("n", "<leader>tt", function()
+  Snacks.terminal(nil, { cwd = LazyVim.root() })
+end, { desc = "Terminal (Root Dir)" })
+map("n", "<c-/>", function()
+  Snacks.terminal(nil, { cwd = LazyVim.root() })
+end, { desc = "Terminal (Root Dir)" })
+map("n", "<c-_>", function()
+  Snacks.terminal(nil, { cwd = LazyVim.root() })
+end, { desc = "which_key_ignore" })
+-- Terminal Mappings
+map("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+map("t", "<c-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
+
