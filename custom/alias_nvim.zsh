116d29649d235d3fa92ac688c15e5644bf8ff81e
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-02-12 10:46:01 +0100

    Remove other nvim distributions

--- custom/alias_nvim.zsh
+++
@@ -1,5 +0,0 @@
-alias v='nvim' # default Neovim config
-alias vz='NVIM_APPNAME=lazyvim nvim' # LazyVim
-alias vc='NVIM_APPNAME=nvchad nvim' # NvChad
-alias va='NVIM_APPNAME=astronvim nvim' # AstroVim
-alias vl='lvim' # LunarVim
