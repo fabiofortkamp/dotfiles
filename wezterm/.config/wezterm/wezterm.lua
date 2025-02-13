c4dae39de85834fe70192fda72a92f4f08063b58
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-02-13 11:16:10 +0100

    Remove wezterm maximizing due to buggy behaviour

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,15 +4,6 @@ local wezterm = require("wezterm")
 -- Maximize all windows on startup
 local mux = wezterm.mux
 
-wezterm.on("gui-attached", function(domain)
-	-- maximize all displayed windows on startup
-	local workspace = mux.get_active_workspace()
-	for _, window in ipairs(mux.all_windows()) do
-		if window:get_workspace() == workspace then
-			window:gui_window():maximize()
-		end
-	end
-end)
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
