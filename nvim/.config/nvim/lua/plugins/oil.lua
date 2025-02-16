7ea73978f1f3e23fd1e8d16fbda0ab6b45262de2
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-02-16 16:23:09 +0100

    Fix oil.nvim configuration to avoid conflicts with tmux

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,15 +1,20 @@
+local oil_opts = {
+  keymaps = {
+    ["<C-h>"] = false,
+    ["<C-l>"] = false,
+  },
+}
 return {
   "stevearc/oil.nvim",
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
   config = function()
-    require("oil").setup()
+    require("oil").setup(oil_opts)
     vim.keymap.set("n", "-", "<cmd>Oil<CR>")
   end,
 }
