8e5ecdc232ac8270a424fb7a14e9df3657b3539a
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-02-16 21:47:57 +0100

    Add fzf config to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,8 @@
 # set up homebrew path
-fish_add_path -gP "/opt/homebrew/bin/" "/opt/homebrew/sbin/";
+fish_add_path -gP /opt/homebrew/bin/ /opt/homebrew/sbin/
 
 # zoxide config
 zoxide init fish | source
 
+# Set up fzf key bindings
+fzf --fish | source
