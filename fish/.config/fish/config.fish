d9156e2e262148f50adfe2e8513768381689bcd9
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-02-16 22:27:19 +0100

    Turn on fish vi keybindings

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,10 @@
 fish_add_path -gP /opt/homebrew/bin/ /opt/homebrew/sbin/
 
 # create local dotfiles scripts dir
-fish_add_path  -gP "$HOME/dotfiles/bin"
+fish_add_path -gP "$HOME/dotfiles/bin"
+
+# set vi keybindings
+set -g fish_key_bindings fish_vi_key_bindings
 
 # zoxide config
 zoxide init fish | source
