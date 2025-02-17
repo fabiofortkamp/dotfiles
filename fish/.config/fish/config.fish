fb97fcaba32b4f10c9b6065a9ca2f8c17ff579ff
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-02-17 21:02:30 +0100

    Add asdf configuration to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,11 +9,26 @@ fish_add_path -gP "$HOME/dotfiles/bin"
 set -g fish_key_bindings fish_vi_key_bindings
 
 # set up tmux-sessionizer keybindings
-bind \cs -M "default" "$HOME/dotfiles/bin/tmux-sessionizer"
-bind \cs -M "insert" "$HOME/dotfiles/bin/tmux-sessionizer"
+bind \cs -M default "$HOME/dotfiles/bin/tmux-sessionizer"
+bind \cs -M insert "$HOME/dotfiles/bin/tmux-sessionizer"
 
 # zoxide config
 zoxide init fish | source
 
 # Set up fzf key bindings
 fzf --fish | source
+
+# ASDF configuration code
+fish_add_path -gP "$HOME/.asdf/bin"
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
