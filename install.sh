af463e0fc7bd170b09326831c7f262e910cde14c
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-02-26 09:52:01 +0100

    Remove curl installation script
    There are too many dependencies that it does not make sense to install
    from source.

--- install.sh
+++ install.sh
@@ -3,10 +3,11 @@
 # Install curl from source if not available
 
 DOTFILES="$HOME/dotfiles"
+
+# important directories that will be use by the installation tools
 mkdir -p "$HOME/.local/{bin,lib}"
 mkdir -p "$HOME/build"
 
-source "$DOTFILES/install_curl.sh"
 # # Install rustup
 # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 #
