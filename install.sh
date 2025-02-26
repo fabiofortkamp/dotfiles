a0b9596e5ac5d55ac850421ee52c5a090a61e640
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-02-26 12:12:03 +0100

    Streamline building Neovim

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 # Install curl from source if not available
+set -e          # Exit immediately if a command exits with non-zero status
+set -u          # Treat unset variables as an error
+set -o pipefail # Return value of a pipeline is the status of the last command that failed
 
 DOTFILES="$HOME/dotfiles"
 
@@ -10,6 +13,9 @@ mkdir -p "$HOME/build"
 
 # Install homebrew
 source "$DOTFILES/brew.sh"
+
+# Install neovim from source
+source "$DOTFILES/build_neovim.sh"
 
 # Link everything together
 source "$DOTFILES/stow.sh"
--- stow.sh
+++ stow.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 
+cd $DOTFILES
 stow mypy ruff julia zsh tmux direnv starship nushell hatch fish nvim wezterm  aerospace git
