9d1dcfaf6793f1a7a6dba88a0410dfa110117284
Fábio Pinto Fortkamp <fabio@fabiofortkamp.com>
2025-03-04 10:23:03 +0100

    Create local dev plugin that will check for all local plugins

--- nvim/.config/nvim/lua/plugins/dev.lua
+++ nvim/.config/nvim/lua/plugins/dev.lua
@@ -1,3 +1,11 @@
-return  {
-  dir = "~/nvim-plugins/present.nvim/"
-}
+local plugins = {}
+local Path = require("pathlib")
+local plugins_dir = Path("~") / "nvim-plugins"
+for this_plugin in plugins_dir:fs_iterdir(false, 1, nil) do
+  print(this_plugin)
+  local plugin = {
+    dir = this_plugin,
+  }
+  table.insert(plugins, plugin)
+end
+return plugins
