f2f7d4e0275b5fa71c6abc9907730d6abdb9bffa
Fábio Pinto Fortkamp <fabio@fabiofortkamp.com>
2025-03-06 08:21:48 +0100

    Add GH cli config

--- stow.sh
+++ stow.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd $DOTFILES
-stow mypy ruff julia zsh tmux direnv starship nushell hatch fish nvim wezterm  aerospace git mise
+stow mypy ruff julia zsh tmux direnv starship nushell hatch fish nvim wezterm  aerospace git mise gh
