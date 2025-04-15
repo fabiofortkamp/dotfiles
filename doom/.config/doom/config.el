6a1a7936be78d955fd523ae8d2b3db9f073f65cf
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-04-15 09:36:34 +0200

    Increase font size in Doom Emacs

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -21,8 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "VictorMono Nerd Font Mono" :size 15 ))
-(setq doom-symbol-font (font-spec :family "VictorMono Nerd Font Mono" :size 15 ))
+(setq doom-font (font-spec :family "VictorMono Nerd Font Mono" :size 16 ))
+(setq doom-symbol-font (font-spec :family "VictorMono Nerd Font Mono" :size 16 ))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
