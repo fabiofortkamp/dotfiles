47e316c99dbe3a1338ab610aefa617ea5ff98a43
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-11-15 09:36:48 +0100

    Include function to highlight text when copying in neovim

--- nvim-advent/.config/nvim-advent/init.lua
+++ nvim-advent/.config/nvim-advent/init.lua
@@ -10,3 +10,14 @@ vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 -- when there's a region selected, executed that with lua
 vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
