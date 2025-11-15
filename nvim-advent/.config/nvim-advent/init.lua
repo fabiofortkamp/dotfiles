8e2e675faaab6bd8cdab000cc22e1e19e5c00b22
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-11-15 09:23:11 +0100

    Create basic commands to reload neovim config

--- nvim-advent/.config/nvim-advent/init.lua
+++ nvim-advent/.config/nvim-advent/init.lua
@@ -1,2 +1,12 @@
-print("advent of neovim")
-print("hello of neovim")
+-- Entry point for the neovim configuration
+
+-- Meta: Basic keybindings for (re-)loading the config
+-- vim is the main table that is accessible through the config
+-- syntax: vim.keymap.set(MODE,KEY,ACTION)
+
+-- reload the current file
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+-- execute the current line using the lua engine - notice the '.' selector for the current line
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+-- when there's a region selected, executed that with lua
+vim.keymap.set("v", "<space>x", ":lua<CR>")
