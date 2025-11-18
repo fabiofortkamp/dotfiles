7da50ad1ebadfc83a2a5025336b68951133b3283
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-11-18 21:31:31 +0100

    Include mini icons and git info in nvim-advent statusbar

--- nvim-advent/.config/nvim-advent/lua/config/plugins/mini.lua
+++ nvim-advent/.config/nvim-advent/lua/config/plugins/mini.lua
@@ -3,8 +3,11 @@ return {
     {
         'nvim-mini/mini.nvim',
         config = function()
-            local statusline = require 'mini.statusline'
-            statusline.setup { use_icons = true }
+		-- enable icons and git information in the statusline
+		require('mini.icons').setup()
+		require('mini.git').setup()
+	    	local statusline = require 'mini.statusline'
+		statusline.setup { use_icons = true }
         end
     }
 }
