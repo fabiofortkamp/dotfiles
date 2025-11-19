d103f1853dd184b3a9ad4c368ba5227d0677e196
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-11-19 07:27:41 +0100

    Replace mini statusline with lualine in nvim-advent

--- nvim-advent/.config/nvim-advent/lua/config/plugins/mini.lua
+++ nvim-advent/.config/nvim-advent/lua/config/plugins/mini.lua
@@ -3,11 +3,6 @@ return {
     {
         'nvim-mini/mini.nvim',
         config = function()
-		-- enable icons and git information in the statusline
-		require('mini.icons').setup()
-		require('mini.git').setup()
-	    	local statusline = require 'mini.statusline'
-		statusline.setup { use_icons = true }
         end
     }
 }
