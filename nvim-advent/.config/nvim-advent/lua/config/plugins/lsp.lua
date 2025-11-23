8220f7a6230c3844542043a5bb591bf65158a353
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-11-23 15:04:02 +0100

    Add go language server to nvim-advent

--- nvim-advent/.config/nvim-advent/lua/config/plugins/lsp.lua
+++ nvim-advent/.config/nvim-advent/lua/config/plugins/lsp.lua
@@ -17,6 +17,7 @@ return {
     config = function()
       -- server configurations
       vim.lsp.enable("lua_ls")
+      vim.lsp.enable("gopls")
 
       -- in nvim 0.11+, the virtual lines have to be turned on
       vim.diagnostic.config({
@@ -24,17 +25,19 @@ return {
       })
 
       -- setting up autocommands when the LSP is attached to the buffer
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('my.lsp', {}),
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("my.lsp", {}),
         callback = function(args)
           local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
           -- Auto-format ("lint") on save.
           -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-          if not client:supports_method('textDocument/willSaveWaitUntil')
-              and client:supports_method('textDocument/formatting') then
-            vim.api.nvim_create_autocmd('BufWritePre', {
-              group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+          if
+            not client:supports_method("textDocument/willSaveWaitUntil")
+            and client:supports_method("textDocument/formatting")
+          then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
               buffer = args.buf,
               callback = function()
                 vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
