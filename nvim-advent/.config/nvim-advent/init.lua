e1e2cc96ec1a650ed9dd8d7cd5369c8abd4707ad
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2025-12-04 20:31:04 +0100

    Set leader and localleader keys in nvim-advent

--- nvim-advent/.config/nvim-advent/init.lua
+++ nvim-advent/.config/nvim-advent/init.lua
@@ -1,5 +1,7 @@
 -- Entry point for the neovim configuration
 
+vim.g.mapleader = "<space>"
+vim.g.maplocalleader = ","
 -- lazy.nvim config
 require("config.lazy")
 
@@ -7,18 +9,18 @@ require("config.lazy")
 -- vim is the main table that is accessible through the config
 -- syntax: vim.keymap.set(MODE,KEY,ACTION)
 -- reload the current file
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<localleader><localleader>x", "<cmd>source %<CR>")
 -- execute the current line using the lua engine - notice the '.' selector for the current line
-vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("n", "<localleader>x", ":.lua<CR>")
 -- when there's a region selected, executed that with lua
-vim.keymap.set("v", "<space>x", ":lua<CR>")
+vim.keymap.set("v", "<localleader>x", ":lua<CR>")
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
