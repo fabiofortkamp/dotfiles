5b033a3cf487e1cd6cd3298bbcf1e907a091fa23
Fábio Pinto Fortkamp <fabio@fabiofortkamp.com>
2026-06-11 14:58:42 +0200

    Switch picker from Telescope to Snacks
    Remove lazyvim_picker = "telescope" override so the snacks_picker
    LazyVim extra takes effect. Drop the telescope catppuccin integration
    since telescope is no longer loaded.

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -38,7 +38,6 @@ return {
         notify = true,
         semantic_tokens = true,
         snacks = true,
-        telescope = true,
         treesitter = true,
         treesitter_context = true,
         which_key = true,
