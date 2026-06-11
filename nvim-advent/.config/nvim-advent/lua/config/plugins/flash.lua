6ae91eab4d75b10372b5a64872a8cd7383029a13
Fábio Pinto Fortkamp <fabio@fabiofortkamp.com>
2026-06-11 14:41:01 +0200

    Remove unused Neovim and Emacs configs
    Delete nvim-advent, nvchad, doom, spacemacs, and chemacs packages.
    Remove the v alias and doom-emacs PATH entry from config.fish, and
    drop nvim-advent and doom from stow.sh. LazyVim (nvim/) is now the
    single editor config.

--- nvim-advent/.config/nvim-advent/lua/config/plugins/flash.lua
+++
@@ -1,8 +0,0 @@
--- the flash plugin highlights search terms
-return {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  ---@type Flash.Config
-  opts = {},
-  keys = {},
-}
--- stow.sh
+++ stow.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd "$HOME/dotfiles" || returns
-stow mypy ruff julia zsh tmux direnv starship nushell hatch fish nvim nvim-advent wezterm git mise gh doom ghostty
+stow mypy ruff julia zsh tmux direnv starship nushell hatch fish nvim wezterm git mise gh ghostty
